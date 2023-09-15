55c3ce8a84e80f86a49b3f51e2cc53f008b310f2
sergioedo <sergio.edo@gmail.com>
2023-09-15 16:14:59 +0200

    click counter

--- nextjs/src/app/page.js
+++ nextjs/src/app/page.js
@@ -1,18 +1,22 @@
+'use client';
+
 // import Image from 'next/image'
+import { useState } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
+  const [clics, setClics] = useState(0);
   return (
     <>
       <h1>Apuesta al clic</h1>
 
       {/* <!-- Contador de clics --> */}
       <div className="counter">
-        Clics: <span id="counter">0</span>
+        Clics: <span id="counter">{clics}</span>
       </div>
 
       {/* <!-- Botón rojo grande y redondo --> */}
-      <button className="red-button">Clic</button>
+      <button className="red-button" onClick={() => setClics(clics + 1)}>Clic</button>
 
       <hr />
 
