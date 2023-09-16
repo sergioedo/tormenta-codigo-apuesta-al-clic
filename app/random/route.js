f2fbbb1530f1299f8bed808b6d813d9a42943f8f
sergioedo <sergio.edo@gmail.com>
2023-09-16 15:12:39 +0200

    invalidate random api

--- app/random/route.js
+++ app/random/route.js
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 import { getUserRandomValue } from '../session';
+import { revalidatePath } from 'next/cache';
 
 // Set Random Value for the current session
 export async function GET(request) {
     const randomValue = await getUserRandomValue();
+    revalidatePath('/random');
     return NextResponse.json({ randomValue });
 }
