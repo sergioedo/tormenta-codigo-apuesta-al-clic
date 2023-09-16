ef84272f5b5e580e29c64e9dbf9283141e9f90c8
sergioedo <sergio.edo@gmail.com>
2023-09-16 09:20:05 +0200

    cleanup page css

--- nextjs/src/app/page.js
+++ nextjs/src/app/page.js
@@ -2,7 +2,6 @@
 
 // import Image from 'next/image'
 import { useState } from 'react'
-import styles from './page.module.css'
 import { apuesta } from './actions'
 
 export default function Home() {
